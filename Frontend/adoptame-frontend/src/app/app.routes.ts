548b694b8fc77275e8deabedcc2db1d0d19f139a
Jorge-Martinez82 <jmartines@gmail.com>
2025-11-07 12:12:26 +0100

    front base creado

--- Frontend/adoptame-frontend/src/app/app.routes.ts
+++ Frontend/adoptame-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Animales } from './pages/animales/animales';
+import { Protectoras } from './pages/protectoras/protectoras';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'animales', component: Animales },
+  { path: 'protectoras', component: Protectoras },
+  { path: '**', redirectTo: '' }
+];
---
+++ Frontend/adoptame-frontend/src/app/pages/protectoras/protectoras.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protectoras',
+  imports: [],
+  templateUrl: './protectoras.html',
+  styleUrl: './protectoras.css',
+})
+export class Protectoras {
+
+}
