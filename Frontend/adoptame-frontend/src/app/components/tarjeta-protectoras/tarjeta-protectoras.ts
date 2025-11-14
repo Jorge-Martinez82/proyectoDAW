ebdc696a3b8faed6b32153adb3dcda0547caf164
Jorge-Martinez82 <jmartines@gmail.com>
2025-11-14 10:46:30 +0100

    trabajando en about

--- Frontend/adoptame-frontend/src/app/components/tarjeta-protectoras/tarjeta-protectoras.ts
+++ Frontend/adoptame-frontend/src/app/components/tarjeta-protectoras/tarjeta-protectoras.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BotonPrincipal } from '../boton-principal/boton-principal';
 
 @Component({
   selector: 'app-tarjeta-protectora',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, BotonPrincipal],
   templateUrl: './tarjeta-protectoras.html',
   styleUrl: './tarjeta-protectoras.css'
 })
